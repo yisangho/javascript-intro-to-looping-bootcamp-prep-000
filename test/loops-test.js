491be175d1ca8d2237dcf718a9c2efc51ddc8839
yisangho <yisangho717@gmail.com>
2018-08-26 03:19:13 +0000

    automatically backed up by learn

--- test/loops-test.js
+++ test/loops-test.js
@@ -31,7 +31,7 @@ describe('loops', () => {
       const [array, t] = makeArray()
       const strangeArray = forLoop(array)
       const testArray = strangeArray.slice(strangeArray.length)
-      
+
       let first = "I am 1 strange loop."
       let rest = "I am 24 strange loops."
 
